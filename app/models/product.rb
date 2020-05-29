6a785f8e359a3b30e5808ec6f3431251bc3d8f45
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-05-29 12:27:04 -0700

    pass test for titleize product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ApplicationRecord
+  
+  
+  
+  
+  
   has_many :reviews,  dependent: :destroy
+  validates :name, presence: true
+  validates :country_of_origin, presence: true
+  validates_length_of :name, maximum: 50
+  before_save(:titleize_product)
+  
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+      self.country_of_origin = self.country_of_origin.titleize
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
-  # it { should validate_presence_of :name }
-  # it { should validate_length_of(:name).is_at_most(50) }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :country_of_origin }
+  it { should validate_length_of(:name).is_at_most(50) }
   
-  # it("titleizes the name of an product, and country of origin") do
-  #   product = Product.create({name: "apple pie"})
-  #   expect(product.name()).to(eq("Apple Pie"))
-  #   # expect(product.country()).to(eq("Usa"))
-  # end
+  it("titleizes the name of an product, and country of origin") do
+    product = Product.create({name: "apple pie", cost: 1, country_of_origin: "usa"})
+    expect(product.name()).to(eq("Apple Pie"))
+    expect(product.country_of_origin()).to(eq("Usa"))
+  end
 end
