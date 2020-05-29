9d563cf3b722b50ddab3c74d40835a5d6552fe0a
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-05-29 14:17:46 -0700

    add in scopes to product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,16 @@
 class Product < ApplicationRecord
   
-  # scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
-  # scope :most_reviews, -> {(
-  #   select("products.id, products.name, count(reviews.id) as reviews_count")
-  #   .joins(:reviews)
-  #   .group("products.id")
-  #   .order("reviews_count DESC")
-  #   .limit(5)
-  #   )}
-  # scope :locally_made, -> { where(country: "United States") }
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+
+  scope :most_reviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(5)
+    )}
+
+  scope :locally_made, -> { where(country_of_origin: "Usa") }
   
   
   
