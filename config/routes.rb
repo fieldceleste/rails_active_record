ed519079459ad58d935681a887d8cb35db5b0752
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-06-05 15:29:50 -0700

    add admin functions to page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   resources :products do
     resources :reviews
   end
