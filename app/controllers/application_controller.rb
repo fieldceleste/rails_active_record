db9949f2693bcbd88decd3f092c9758ddb291077
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-06-05 16:44:54 -0700

    add in admin user functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
 
   # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name])
+  #   # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  #   # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   # end
 end
