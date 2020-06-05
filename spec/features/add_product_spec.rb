24c55c6be614d40599b53863587225552b69758c
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-06-05 09:36:50 -0700

    pass second integration spec

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -10,4 +10,11 @@ describe "the add a product process" do
     click_on 'Create Product'
     expect(page).to have_content 'Cheese'
   end
+
+  it "gives an error when no name is entered" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Country of origin can't be blank"
+  end
 end
